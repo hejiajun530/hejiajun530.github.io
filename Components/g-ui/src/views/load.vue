f2abc94e4e78af7aa9b0cf0faa9fcebff9ad4e94
hejiajun530@126.com <hejiajun530@126.com>
2020-09-03 17:53:53 +0800

    'edit'

--- Components/g-ui/src/views/load.vue
+++ Components/g-ui/src/views/load.vue
@@ -5,6 +5,13 @@
     <button @click="handleClickLoadShow2">loading样式2</button>
     <button @click="handleClickLoadShow3">loading样式3</button>
     <button @click="handleClickLoadShow4">loading样式4</button>
+    <div class="load-mask">
+      <div class="load">
+        <div class="load-item"></div>
+        <div class="load-item"></div>
+        <div class="load-item"></div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -48,3 +55,52 @@ export default {
   }
 };
 </script>
+<style scoped>
+.load-mask {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.6);
+}
+.load {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 150px;
+  height: 50px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+.load-item {
+  width: 15px;
+  height: 15px;
+  border: 3px solid #ffc200;
+  /* border: 3px solid #ffffff; */
+  border-radius: 50%;
+  animation: scaleLoad 1.5s ease 0ms infinite;
+  transform: scale(0);
+  box-sizing: border-box;
+}
+.load-item:nth-child(1) {
+  animation-delay: 0.3s;
+}
+.load-item:nth-child(2) {
+  animation-delay: 0.6s;
+}
+.load-item:nth-child(3) {
+  animation-delay: 0.9s;
+}
+@keyframes scaleLoad {
+  50% {
+    transform: scale(1);
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+}
+</style>
--- Components/react/fristreact/src/App.js
+++ Components/react/fristreact/src/App.js
@@ -3,38 +3,70 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  var data = '123123';
-  console.log(data);
-  const list = [1, 2, 3, 4, 5, 6];
-  const listItem = list.map((item) =>
-    <li key={item.toString()}>
-      {item}
-    </li>
-  );
-  function handleClickAlert() {
-    alert('test');
-    console.log('test');
-  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p onClick={handleClickAlert}>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="./index.js"
-          rel="noopener noreferrer"
-        >
-          index{data}
-        </a>
-        <ul>
-          {listItem}
-        </ul>
+        <Game />
       </header>
     </div>
   );
+}
+function Square(props) {
+  return (
+    <button className="board-row-btn" onClick={props.onClick}>
+      {props.num}
+    </button>
+  )
+}
+class Board extends React.Component {
+  renderSquare(i) {
+    return (
+      <Square num={i} onClick={() => this.props.onClick(i)} />
+    )
+  }
+  render() {
+    return (
+      <div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    )
+  }
+}
+
+class Game extends React.Component {
+  handleClickChoose(i) {
+    console.log(i);
+  }
+  render() {
+    const squares = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    return (
+      <Board onClick={i => this.handleClickChoose(i)} />
+    )
+  }
 }
 
 export default App;
