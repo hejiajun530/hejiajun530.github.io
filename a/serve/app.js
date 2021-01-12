46f6312e89df52389f157a7aa6f20f5464111fc9
hejiajun530 <hejiajun530@126.com>
2021-01-12 12:25:47 +0800

    1

---
+++ a/serve/app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const https = require('https');
+const fs = require('fs');
+const options = {
+  key: fs.readFileSync('./ssl/2_tyq121.top.key'),
+  cert: fs.readFileSync('./ssl/1_tyq121.top_bundle.crt')
+}
+
+// 历史组件
+var history = require('connect-history-api-fallback')
+app.use(history())
+
+// 全局生成一个密钥  用于用户的token
+app.set('secret', 'asfasdfgsdagdsfg')
+
+app.use(require('cors')())
+app.use(express.json())
+
+app.use('/', express.static(__dirname + '/abc'));
+
+const server = https.createServer(options, app);
+server.listen(3000, () => {
+  console.log('http://www.tyq121.top:3000')
+})
+// app.listen(3000, () => {
+//   console.log('http://localhost:3000')
+// })
